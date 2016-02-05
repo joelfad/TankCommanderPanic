9fe2bc19eeb2cb83bd22e9ceff361f7634c0c9ee
Leonardo Banderali <lbanderali@yahoo.com>
2016-02-05 16:20:54 -0700

    Improve UDP Client.

--- udp_client_cpp/main.cpp
+++ udp_client_cpp/main.cpp
@@ -1,26 +1,29 @@
-//
-// blocking_udp_echo_client.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+/*
+Project:  Server
+File:  main.cpp
+Author:  Joel McFadden
+Description:  This is a simple TCP client to work with the provided TCP/UDP server.
+Notes:  Code was inspired from some examples provided with the Boost.Asio library.
+    (http://www.boost.org/doc/libs/1_60_0/doc/html/boost_asio/examples/cpp11_examples.html)
+*/
 
-#include <cstdlib>
-#include <cstring>
+// project headers
+#include <char_array.hpp>
+
+// c++ standard libraries
 #include <iostream>
+
+// boost libraries
 #include <boost/asio.hpp>
 
 using boost::asio::ip::udp;
 
-enum { max_length = 1024 };
+constexpr std::size_t max_length = 1024;
 
 int main(int argc, char* argv[]) {
     try {
         if (argc != 3) {
-            std::cerr << "Usage: blocking_udp_echo_client <host> <port>\n";
+            std::cerr << "Usage: udp_client_cpp <host> <port>\n";
             return 1;
         }
 
@@ -33,18 +36,18 @@ int main(int argc, char* argv[]) {
 
         while (true) {
             std::cout << "Please enter a message to be sent to the server ('logout' to terminate): ";
-            char request[max_length];
-            std::cin.getline(request, max_length);
-            if (strcmp(request, "logout") == 0)
+            char_array<max_length> request;
+            std::cin.getline(request.data(), max_length);
+            if (request == "logout")
                 break;
-            size_t request_length = std::strlen(request);
+            size_t request_length = request.length();
             s.send_to(boost::asio::buffer(request, request_length), endpoint);
 
-            char reply[max_length];
+            char_array<max_length> reply;
             udp::endpoint sender_endpoint;
             size_t reply_length = s.receive_from(boost::asio::buffer(reply, max_length), sender_endpoint);
             std::cout << "Server: ";
-            std::cout.write(reply, reply_length);
+            std::cout.write(reply.data(), reply_length);
             std::cout << "\n";
         }
     }
