567d88f906e4e8f4f2530068a09f44702bb35e8a
Joel McFadden <mcf.joel@gmail.com>
2016-02-05 16:31:03 -0700

    Clean up the build process.

--- build.sh
+++ build.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
-# This script is provided to automate building the TCP/UDP Server and TCP Client
-# for CPSC 441 Assignment 1. It was created by Team 36 on Feb. 5, 2016.
+
+# Project: CPSC 441, Assignment 1
+# File: build.sh
+# Author: Joel McFadden
+# Created: February 4, 2016
+# Modified: February 4, 2016
+
+# Description This script is provided to automate building the TCP/UDP server
+#             and clients for CPSC 441 Assignment 1.
+
+# Relative Paths
+SDIR="server"
+TDIR="tcp_client_cpp"
+UDIR="udp_client_cpp"
 
 # Build the TCP/UDP Server
 echo "Building TCP/UDP Server..."
-cd server
-cmake .
-make
-ln -s server/server ../SelectServer
+cmake -B$SDIR/build -H$SDIR
+cd $SDIR/build && make
+cd ../../
 
 # Build the TCP Client
 echo "Building TCP Client..."
-cd ../tcp_client_cpp
-cmake .
-make
-ln -s tcp_client_cpp/tcp_client_cpp ../TCPClient
+cmake -B$TDIR/build -H$TDIR
+cd $TDIR/build && make
+cd ../../
+
+# Build the UDP Client
+echo "Building UDP Client..."
+cmake -B$UDIR/build -H$UDIR
+cd $UDIR/build && make
+cd ../../
+
+# Create symlinks
+ln -s $SDIR/build/server SelectServer
+ln -s $TDIR/build/tcp_client_cpp TCPClient
+ln -s $UDIR/build/udp_client_cpp UDPClient
