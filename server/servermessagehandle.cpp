d5eac6e0c97a24ee0aca89c931707f18df4f21fe
Leonardo Banderali <lbanderali@yahoo.com>
2016-03-27 11:43:46 -0600

    Add abstract server message handle.

---
+++ server/servermessagehandle.cpp
@@ -0,0 +1,11 @@
+/*
+Project:  Tank Commander Panic
+File:  servermessagehandle.hpp
+Author:  Leonardo Banderali
+Description:  A handle for messages sent by the server to clients.
+*/
+
+#include "servermessagehandle.hpp"
+
+protocol::ServerMessageHandle::ServerMessageHandle(){
+}
---
+++ server/servermessagehandle.hpp
@@ -0,0 +1,28 @@
+/*
+Project:  Tank Commander Panic
+File:  servermessagehandle.hpp
+Author:  Leonardo Banderali
+Description:  A handle for messages sent by the server to clients.
+*/
+
+#ifndef SERVERMESSAGEHANDLE_HPP
+#define SERVERMESSAGEHANDLE_HPP
+
+#include "message.hpp"
+
+namespace protocol {
+
+class ServerMessageHandle {
+    public:
+        ServerMessageHandle();
+        ~ServerMessageHandle() noexcept {}
+
+        virtual auto to_msg() const = 0 -> Message;
+        /*  return message containing the fields specified by this handle */
+
+    private:
+};
+
+}
+
+#endif // SERVERMESSAGEHANDLE_HPP
