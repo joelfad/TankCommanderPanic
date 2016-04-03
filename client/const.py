5148f9ae5dd6edfdd1218b4fe6d73bd92bcc1c95
Joel McFadden <mcf.joel@gmail.com>
2016-04-02 19:34:43 -0600

    Add hud sprites and tank data to constants.

--- client/const.py
+++ client/const.py
@@ -3,7 +3,9 @@
 # File: const.py
 # Author: Joel McFadden
 # Created: March 29, 2016
-# Modified: April 1, 2016
+# Modified: April 2, 2016
+
+import sfml as sf
 
 #############
 # Constants #
@@ -12,31 +14,77 @@
 # piece_type mapped to tile_id
 sprite_map = \
 {
-     2: 384,    # brick
-     3: 384,    # brick
-     4: 384,    # brick
-     5: 384,    # brick
-     6: 384,    # brick
-     7: 384,    # brick
-     8: 480,    # health
-     9: 478,    # ammo
+    2:  384,    # brick
+    3:  384,    # brick
+    4:  384,    # brick
+    5:  384,    # brick
+    6:  384,    # brick
+    7:  384,    # brick
+
+    8:  480,    # health
+
+    9:  478,    # ammo
+
     10: 0,      # decoration
+
     16: 484,    # red commander
     17: 485,    # red interceptor
     18: 486,    # red eliminator
     19: 487,    # red negotiator
+
     20: 505,    # blue commander
     21: 506,    # blue interceptor
     22: 507,    # blue eliminator
     23: 508,    # blue negotiator
+
     24: 526,    # yellow commander
     25: 527,    # yellow interceptor
     26: 528,    # yellow eliminator
     27: 529,    # yellow negotiator
+
     28: 547,    # green commander
     29: 548,    # green interceptor
     30: 549,    # green eliminator
     31: 550,    # green negotiator
+}
+
+COMMANDER_RANGE     = 5 # TODO: Get correct value
+INTERCEPTOR_RANGE   = 8 # TODO: Get correct value
+ELIMINATOR_RANGE    = 3 # TODO: Get correct value
+NEGOTIATOR_RANGE    = 1 # TODO: Get correct value
+
+# piece_type mapped to tank data
+tank_map = \
+{
+    16: ("Commander", COMMANDER_RANGE, sf.Color.RED),
+    20: ("Commander", COMMANDER_RANGE, sf.Color.BLUE),
+    24: ("Commander", COMMANDER_RANGE, sf.Color.YELLOW),
+    28: ("Commander", COMMANDER_RANGE, sf.Color.GREEN),
+
+    17: ("Interceptor", INTERCEPTOR_RANGE, sf.Color.RED),
+    21: ("Interceptor", INTERCEPTOR_RANGE, sf.Color.BLUE),
+    25: ("Interceptor", INTERCEPTOR_RANGE, sf.Color.YELLOW),
+    29: ("Interceptor", INTERCEPTOR_RANGE, sf.Color.GREEN),
+
+    18: ("Eliminator", ELIMINATOR_RANGE, sf.Color.RED),
+    22: ("Eliminator", ELIMINATOR_RANGE, sf.Color.BLUE),
+    26: ("Eliminator", ELIMINATOR_RANGE, sf.Color.YELLOW),
+    30: ("Eliminator", ELIMINATOR_RANGE, sf.Color.GREEN),
+
+    19: ("Negotiator", NEGOTIATOR_RANGE, sf.Color.RED),
+    23: ("Negotiator", NEGOTIATOR_RANGE, sf.Color.BLUE),
+    27: ("Negotiator", NEGOTIATOR_RANGE, sf.Color.YELLOW),
+    31: ("Negotiator", NEGOTIATOR_RANGE, sf.Color.GREEN),
+}
+
+hud_sprites = \
+{
+    "range_n": 510,
+    "range_e": 531,
+    "range_s": 530,
+    "range_w": 509,
+    "center":  488,
+    "pan":     489,
 }
 
 #############
@@ -57,6 +105,9 @@ tilewidth = 32      # pixels wide per tile graphic
 tileheight = 32     # pixels high per tile graphic
 tilecount = 567     # total number of tiles in texture
 columns = 21        # columns in texture
+
+# mock properties
+tank_range = 5
 
 # mock tanks
 red_commander = (1000, 1, 1, 16, 20)
