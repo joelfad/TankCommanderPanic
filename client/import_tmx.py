4c9d8827e92e643ecec7f9235fc1715352d26cec
Marissa <badenm42@gmail.com>
2016-04-03 17:38:18 -0600

    Add some commenting.

--- client/import_tmx.py
+++ client/import_tmx.py
@@ -4,36 +4,48 @@ import os
 import tmx
 
 def load_map_file(id_, version):
+    # define map directory
     maps_directory = "../resources/graphics/maps"
+    # choose file name beginning and end
     filepathbeginning = str(id_) + '_' + str(version)
     filepathend = '.tmx'
+    # set initial map file search variables
     map_found = False
     map_file = None
+    # iterate through all files in maps_directory
     for file_name in os.listdir(maps_directory):
+        # look for file with the format filepathbeginning*filepathend
         if file_name.startswith(filepathbeginning) and file_name.endswith(filepathend):
+            # check if the map is valid
             map_found = validate_map_version(maps_directory, file_name, id_, version, map_found)
             if map_found == True:
                 map_file = file_name
 
     if map_file == None:
+        # if there are no valid map files
         raise FileNotFoundError("Map with id " + str(id_) + " and version " + str(version) + " cannot be found.")
     else:
+        # read valid map
         read_map_file(map_file)
 
 def validate_map_version(maps_directory, filename, id_, version, map_found):
-    #check version information
+    # load the map file
     file_path = maps_directory + '/' + filename
     map = tmx.TileMap.load(file_path)
+
+    # get the map_version from the map file
     map_version = int(float(map.version))
 
-    #check id information
+    # get the map id from the map file
     map_id = None;
     properties = map.properties
     for map_property in properties:
         if map_property.name == 'id':
             map_id = int(float(map_property.value));
-    #if map is valid
+
+    # check if map is valid
     if map_version == version and map_id == id_:
+        # if a valid map was found before this, raise an exception
         if map_found == True:
             raise RuntimeError("Duplicate map files found.")
         else:
