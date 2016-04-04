8ea0fac60a533d682b9ba465afa31282b7faeec2
Marissa <badenm42@gmail.com>
2016-04-03 18:03:21 -0600

    Refactor to use global variables map found, file and dir.

--- client/import_tmx.py
+++ client/import_tmx.py
@@ -3,39 +3,41 @@
 import os
 import tmx
 
+MAPS_DIRECTORY = '';
+MAP_FOUND = False;
+MAP_FILE = None;
+
 def load_map_file(id_, version):
+    global MAPS_DIRECTORY, MAP_FOUND, MAP_FILE
     # define map directory
-    maps_directory = "../resources/graphics/maps"
+    MAPS_DIRECTORY = "../resources/graphics/maps"
     # choose file name beginning and end
     filepathbeginning = str(id_) + '_' + str(version)
     filepathend = '.tmx'
-    # set initial map file search variables
-    map_found = False
-    map_file = None
     # iterate through all files in maps_directory
-    for file_name in os.listdir(maps_directory):
+    for file_name in os.listdir(MAPS_DIRECTORY):
         # look for file with the format filepathbeginning*filepathend
         if file_name.startswith(filepathbeginning) and file_name.endswith(filepathend):
             # check if the map is valid
-            map_found = validate_map_version(maps_directory, file_name, id_, version, map_found)
-            if map_found == True:
-                map_file = file_name
+            validate_map_version(file_name, id_, version)
+            if MAP_FOUND == True:
+                MAP_FILE = file_name
 
-    if map_file == None:
+    if MAP_FILE == None:
         # if there are no valid map files
         raise FileNotFoundError("Map with id " + str(id_) + " and version " + str(version) + " cannot be found.")
     else:
         # read valid map
-        read_map_file(map_file)
+        read_map_file()
 
-def validate_map_version(maps_directory, filename, id_, version, map_found):
+def validate_map_version(filename, id_, version):
+    global MAP_FOUND
     # load the map file
-    file_path = maps_directory + '/' + filename
+    file_path = MAPS_DIRECTORY + '/' + filename
     map = tmx.TileMap.load(file_path)
 
     # get the map_version from the map file
     map_version = int(float(map.version))
-
     # get the map id from the map file
     map_id = None;
     properties = map.properties
@@ -46,15 +48,15 @@ def validate_map_version(maps_directory, filename, id_, version, map_found):
     # check if map is valid
     if map_version == version and map_id == id_:
         # if a valid map was found before this, raise an exception
-        if map_found == True:
+        if MAP_FOUND == True:
             raise RuntimeError("Duplicate map files found.")
         else:
-            return True;
+            MAP_FOUND = True;
     #if map is invalid
     else:
-        return False;
+        MAP_FOUND = False;
 
-def read_map_file(mapfile):
+def read_map_file():
 
 
     '''
